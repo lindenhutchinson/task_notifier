c95bf92f0fa46a39d5da2e2dd3f9084122d60bcb
Linden Hutchinson <hutchinson.linden@gmail.com>
2020-08-18 17:53:28 +1000

    can now toggle verbosity

--- single_signon.py
+++ single_signon.py
@@ -6,34 +6,41 @@ import urllib.parse
 
 
 # use this constant to allow selenium more time to find elements
-SLEEP_TIME = 4
+SLEEP_TIME = 10
 
 # used to determine how many times selenium should retry finding an element on the browser
 SEL_RETRIES = 3
 
+
 class SingleSignon:
-    def __init__(self, user, p, selenium_dir, run_headless):
+    def __init__(self, user, p, selenium_dir, run_headless, verbose):
         self.username = user
         self.password = p
         self.sel_dir = selenium_dir
         self.run_headless = run_headless
+        self.verbose = verbose
         self.driver = self.make_web_driver()
 
-
     # create selenium driver to access ontrack webpage
+
     def make_web_driver(self):
-        print("Loading selenium...")
+        if self.verbose:
+            print("Loading selenium...")
         chrome_options = webdriver.ChromeOptions()
         if self.run_headless:
             chrome_options.add_argument('--headless')
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument("--enable-javascript")
+        chrome_options.add_argument("--no-sandbox");
+        chrome_options.add_argument("--disable-dev-shm-usage");
         driver = webdriver.Chrome(
             options=chrome_options, executable_path=self.sel_dir)
 
-        print("Attempting to load page")
+        if self.verbose:
+            print("Accessing ontrack page that will redirect us to authentication")
         driver.get("https://ontrack.deakin.edu.au/#/home")
-        print("Loaded! Waiting a little longer for good measure")
+        if self.verbose:
+            print("Page loaded. We will sleep for {} seconds now".format(SLEEP_TIME))
         time.sleep(SLEEP_TIME)
         return driver
 
@@ -43,12 +50,14 @@ class SingleSignon:
             try:
                 return self.driver.find_element_by_id(id)
             except:
-                count+=1
-                print("Attempt {}. Couldn't find {}. Will retry in {} seconds".format(count,id,SLEEP_TIME))
+                count += 1
+                if self.verbose:
+                    print("Attempt {}. Couldn't find {}. Will retry in {} seconds".format(count, id, SLEEP_TIME))
                 time.sleep(SLEEP_TIME)
 
 
-        print("Couldn't find {} after max retries. Quitting...".format(id))
+        if self.verbose:
+            print("Couldn't find {} after max retries. Quitting...".format(id)) 
         sys.exit(0)
 
 
@@ -59,41 +68,57 @@ class SingleSignon:
                 return self.driver.find_element_by_class_name(name)
             except:
                 count+=1
-                print("Attempt {}. Couldn't find {}. Will retry in {} seconds".format(count,name,SLEEP_TIME))
+                if self.verbose:
+                    print("Attempt {}. Couldn't find {}. Will retry in {} seconds".format(count,name,SLEEP_TIME)) 
                 time.sleep(SLEEP_TIME)
 
 
-        print("Couldn't find {} after max retries. Quitting...".format(name))
+        if self.verbose:
+            print("Couldn't find {} after max retries. Quitting...".format(name)) 
         sys.exit(0)
 
-        print("oops!!!")
+    def is_text_present(self, text):
+        return str(text) in self.driver.page_source
+
     # login to the ontrack site via selenium
     def login_ontrack(self):
-        print("Logging in to ontrack...")
-        try:
-            name_input = self.try_get_ele_by_id("username")
-        except:
-            print("Unable to find element the first time, trying it again!")
-            time.sleep(SLEEP_TIME)
-            name_input = self.try_get_ele_by_id("username")
-
+        if self.verbose:
+            print("Logging in to ontrack...") 
+        
+        name_input = self.try_get_ele_by_id("username")
         name_input.send_keys(self.username)
-        pass_input = self.try_get_ele_by_id("password")
+        pass_input = self.try_get_ele_by_id("password") 
         pass_input.send_keys(self.password.decode())
         signin_button = self.try_get_ele_by_class_name("btn--login")
         signin_button.click()
-        print("Logged in")
+        if self.verbose:
+            print("Have clicked the first sign in button. Will now sleep for {} seconds".format(SLEEP_TIME)) 
+
         time.sleep(SLEEP_TIME)
+
+        if self.is_text_present('The service you are trying to access is connected to the Australian Access Federation. Select your organisation below to log in.'):
+            if self.verbose:
+                print("We have logged in")
+        else:
+            if self.verbose:
+                print("Something has gone wrong with the deakin authentication")
+            raise ValueError('Unable to login to Deakin Single Signon.')
+
+
+
         signin_button = self.try_get_ele_by_class_name("btn--login")
         signin_button.click()
-        print("Clicking through")
+        if self.verbose:
+            print("Just clicked the second sign in button. Will now sleep for {} seconds".format(SLEEP_TIME))
         time.sleep(SLEEP_TIME)
-        self.logged_in = True
 
-        print("hopefully we have some authentication now, let's try access ontrack...")
+        self.logged_in = True
+        if self.verbose:
+            print("Ideally we should be authenticated with DSS now. Will now access the ontrack homepage again")
         self.driver.get("https://ontrack.deakin.edu.au/#/home")
-        print("loaded ontrack page, we will give it plenty of time to load")
-        time.sleep(SLEEP_TIME)
+        if self.verbose:
+            print("Selenium has loaded the ontrack page, but we will give it {} seconds to fully load".format(SLEEP_TIME*2))
+        time.sleep(SLEEP_TIME*2)
 
     # after logging in to ontrack, capture the created auth token so we can access the API
     def get_auth_token(self):
@@ -108,12 +133,17 @@ class SingleSignon:
                 break
 
         if not auth:
-            print("ERROR: Couldn't find auth token in cookies")
-            sys.exit(0)
+            if self.verbose:
+                print("ERROR: Couldn't find auth token in cookies")
 
-        identity = json.loads(urllib.parse.unquote(auth))
-        auth_token = identity['authenticationToken']
-        print("We're in business, we've got the auth token")
+            raise ValueError("Couldn't find the ontrack auth token in the browser cookies")
+
+
+        deakin_id = json.loads(urllib.parse.unquote(auth))
+        auth_token = deakin_id['authenticationToken']
+        if self.verbose:
+            print("We're in business, we've got the auth token. The next part will be much faster")
+
         self.driver.quit()
 
         return auth_token
